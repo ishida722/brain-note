acfd7b6d2d12c1984c1058029110a804e2241098
t-ishida <t-ishida@ditect.co.jp>
2020-09-11 12:00:56 +0900

    ノートリスト作成中

---
+++ src/components/NoteList.vue
@@ -0,0 +1,15 @@
+<template>
+  <ul>
+    <li v-for="(message, index) in messages" :key="index">{{ message.content }} index:{{ index }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "NoteList",
+  props: {
+    noteBookId: String,
+  },
+  mounted() {},
+};
+</script>
