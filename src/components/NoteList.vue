0b8581ade5d7893dacb8f6d7e59399a562217823
t-ishida <t-ishida@ditect.co.jp>
2020-09-11 17:32:49 +0900

    ノートリスト作成中

--- src/components/NoteList.vue
+++ src/components/NoteList.vue
@@ -1,15 +1,40 @@
 <template>
   <ul>
-    <li v-for="(message, index) in messages" :key="index">{{ message.content }} index:{{ index }}</li>
+    <li v-for="note in notes" :key="note.name">{{ note.name }}</li>
   </ul>
 </template>
 
 <script>
+import firebase from "../firebase.js";
+
 export default {
   name: "NoteList",
+  data() {
+    return {
+      noteIds: [],
+      notes: [],
+    };
+  },
   props: {
     noteBookId: String,
   },
-  mounted() {},
+  computed: {
+
+  },
+  methods: {},
+  mounted() {
+    this.noteBookId;
+    firebase.database().ref("notebooks" + "/" + this.noteBookId)
+    .on('value', (snapshot) =>
+    {
+        this.noteIds = snapshot.val().notes;
+    for (const id in this.noteIds) {
+        var noteId = this.noteIds[id]
+        console.log('for');
+        console.log(noteId);
+        firebase.database().ref('notes/' + noteId).on('value', (snapshot) => this.notes.push[snapshot.val()]);
+    };
+    }
+  }
 };
 </script>
